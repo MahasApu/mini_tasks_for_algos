7a914bc10058f1a8739c41f08cba2609be244ce3
MahasApu <maryvask0@yandex.ru>
2023-02-13 20:05:55 +0700

    add func for length

--- tasks/task2.py
+++ tasks/task2.py
@@ -1,8 +1,15 @@
+def getlen(num):
+    l = 1
+    while num // 10:
+        num//=10
+        l+=1
+    return l
+
 def karatsuba(x, y):
-    if x < 10 or y < 10:
+    if x // 10 == 0 or y // 10 == 0:
         return x * y
 
-    length = max(len(str(x)), len(str(y)))
+    length = max(getlen(x), getlen(y))
     if length % 2 == 1:
         length -= 1
 
@@ -11,11 +18,8 @@ def karatsuba(x, y):
     b = x % 10 ** half_part
     c = y // 10 ** half_part
     d = y % 10 ** half_part
-    print(a,b,c,d,'.................', x, y)
 
     ac = karatsuba(a, c)
     bd = karatsuba(b, d)
     ad_plus_bc = karatsuba(a + b, c + d) - ac - bd
     return ac * 10 ** length + ad_plus_bc * 10 ** half_part + bd
-
-print(karatsuba(1234212345621,3333333))
