4af47f3ec02624bc969e449ada0a046b95b5ae47
MahasApu <maryvask0@yandex.ru>
2023-02-13 19:59:42 +0700

    add func for length

--- tasks/task2.py
+++ tasks/task2.py
@@ -11,8 +11,11 @@ def karatsuba(x, y):
     b = x % 10 ** half_part
     c = y // 10 ** half_part
     d = y % 10 ** half_part
+    print(a,b,c,d,'.................', x, y)
 
     ac = karatsuba(a, c)
     bd = karatsuba(b, d)
     ad_plus_bc = karatsuba(a + b, c + d) - ac - bd
     return ac * 10 ** length + ad_plus_bc * 10 ** half_part + bd
+
+print(karatsuba(1234212345621,3333333))
