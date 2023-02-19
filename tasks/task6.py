2c8fb9a1072572bdc1e315368b9af0e18ef4a333
MahasApu <maryvask0@yandex.ru>
2023-02-20 01:56:21 +0700

    add uncompleted bash script

---
+++ tasks/task6.py
@@ -0,0 +1,29 @@
+
+def wiggle_sort(nums):
+
+    pivot = len(nums) // 2
+    while pivot:
+        for i in range(pivot, len(nums)):
+            temp = nums[i]
+            j = i
+            while j >= pivot and nums[j - pivot] > temp:
+                nums[j] = nums[j - pivot]
+                j -= pivot
+            nums[j] = temp
+        pivot //= 2
+
+    right = len(nums) - 1
+    pivot = (len(nums) - 1) // 2
+
+    temp = [0 for i in range(len(nums))]
+
+    for i in range(1, len(nums), 2):
+            temp[i] = nums[right]
+            right -= 1
+    for i in range(0, len(nums), 2):
+            temp[i] = nums[pivot]
+            pivot -= 1
+
+    for i in range(len(nums)):
+        nums[i] = temp[i]
+    return nums
