011370aa0c10f7c261b751b1a48f452ca4231d1b
MahasApu <maryvask0@yandex.ru>
2023-03-05 15:56:07 +0700

    change array of commits

--- tasks/git_bisect.sh
+++ tasks/git_bisect.sh
@@ -2,51 +2,50 @@
 
 cd $1
 
-#print all commits
+# print all commits
 git log --pretty=format:'%h'
+
+# read the latest good and bad commit
 echo
 echo "Enter two commits"
-
-
-#read the latest good and bad commit
 read start end
-good=`git log $start..HEAD --pretty=oneline | wc -l`
-bad=`git log $end..HEAD --pretty=oneline | wc -l`
-echo $good $bad
 
-#expand range
-if [ $good -gt $bad ]; then
-    good=$[ $good + 1 ]
-    bad=$[ $bad - 1 ]
-else
-    bad=$[ $bad + 1 ]
-    good=$[ $good - 1 ]
-fi
-
-# all commits
-commits=($(git log --pretty=format:'%h'))
-
-echo
-echo "Enter the command to execute"
+# read command to execute
+echo "Enter the command to execute: "
 read command
 
-pivot=$[($good + $bad) / 2]
-wanted=$bad
+# get an array of all commits from start commit to end comm>
+all_commits=$(git log $start..$end --pretty=format:'%h')
 
-while [ $[$good - $bad] -lt -1 ] | [ $[$good - $bad] -gt 1 ]
-do
-    git checkout ${commits[$pivot]}  -q > /dev/null
-    $command
-    result=$?
+# add start commit to the end
+commits="${all_commits} $start"
+commits=($(echo $commits))
 
-    if [ $result -eq 0 ]; then
-        good=$pivot
-    else
-        bad=$pivot
-        wanted=${commits[$pivot]}
-    fi
-    git checkout - -q > /dev/null
-    pivot=$[($good + $bad) / 2]
+# init good and bad commits as last and first indexes in array
+good=${#commits[@]}
+bad=0
+
+pivot=$((($good + $bad) / 2))
+wanted=${commits[bad]}
+
+while [ $(($good - $bad)) -gt 1 ]; do
+
+  git checkout ${commits[$pivot]} -q >/dev/null
+  $command
+  result=$?
+
+  if [ $result -eq 0 ]; then
+    good=$pivot
+
+  else
+    bad=$pivot
+    wanted=${commits[$pivot]}
+  fi
+
+  git checkout - -q >/dev/null
+  pivot=$((($good + $bad) / 2))
+
 done
+
 echo "bad commit is"
 echo $wanted
