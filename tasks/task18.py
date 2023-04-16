cd829f5d50c3535604d2096a6f834a5d0fccba7b
MahasApu <maryvask0@yandex.ru>
2023-04-16 23:27:48 +0700

    add solution for RPN and tests

---
+++ tasks/task18.py
@@ -0,0 +1,70 @@
+from typing import List
+
+expressions = []
+with open(r"C:\Users\vaskm\PycharmProjects\tasks_for_algos\tasks\input_task18.txt", "r") as file:
+    for line in file.readlines():
+        expressions.append(line.split())
+
+
+def ret_priority(operator: str) -> int:
+    operator_priority = {
+        "(": 0,
+        ")": 1,
+        "*": 2, "/": 2, "%": 2,
+        "+": 3, "-": 3,
+        "<<": 4, ">>": 4,
+        "<": 5, "<=": 5, ">": 5, ">=": 5,
+        "&": 6,
+        "|": 7,
+        "&&": 8,
+        "||": 9,
+        "^": 10
+    }
+    return operator_priority[operator]
+
+
+def to_RPN(expression: List[str]) -> List[str]:
+    right_assoc_operators = {"^"}
+    all_operators = ["^", "*", "/", "%", "+", "-", "<<", ">>", "<", "<=", ">", ">=", "&", "|", "&&", "||", ")", "("]
+    stack = []
+    result = []
+    for elem in expression:
+        if elem in all_operators:
+            if elem == "(":
+                stack.append(elem)
+            else:
+                while stack and (
+                        ret_priority(stack[-1]) >= ret_priority(elem)
+                        and elem not in right_assoc_operators
+                ):
+                    result.append(stack.pop())
+                if elem == ')':
+                    stack.pop()
+                else:
+                    stack.append(elem)
+        else:
+            result.append(elem)
+
+    while stack:
+        result.append(stack.pop())
+    return result
+
+
+def to_calc(expression: List[str]) -> int:
+    operators = {"^": (lambda x, y: x ** y), "*": (lambda x, y: x * y), "/": (lambda x, y: x // y),
+                 "%": (lambda x, y: x % y), "+": (lambda x, y: x + y), "-": (lambda x, y: x - y),
+                 "<<": (lambda x, y: x << y), ">>": (lambda x, y: x << y), "<": (lambda x, y: x < y),
+                 "<=": (lambda x, y: x <= y), ">": (lambda x, y: x > y), ">=": (lambda x, y: x >= y),
+                 "&": (lambda x, y: x & y), "|": (lambda x, y: x | y), "&&": (lambda x, y: x and y),
+                 "||": (lambda x, y: x or y)}
+    polish = to_RPN(expression)
+    stack = []
+    for elem in polish:
+        if elem in operators:
+            elem2, elem1 = stack.pop(), stack.pop()
+            result = operators[elem](elem1, elem2)
+        else:
+            result = int(elem)
+        stack.append(result)
+
+    return stack.pop()
---
+++ tests/task18_test.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main
+from tasks.task18 import to_calc
+
+
+class EighteenTask(TestCase):
+    def checker(self, expr: str, for_check: str):
+        self.assertEqual(to_calc(expr.split()), eval(for_check))
+
+    def test_first(self):
+        self.checker('4 + 8 + 1 ^ 32', '4 + 8 + 1 ** 32')
+
+    def test_second(self):
+        self.checker('( 11 / 11 ) * 12 - ( 1 + 3 ) ^ 2', '( 11 //11 ) * 12 - (1 + 3) ** 2')
+
+    def test_third(self):
+        self.checker('( 1 + ( ( ( ( 33 & 11 ) * 2 ) ) - 1 ) )', '( 1 + ( ( ( (33 & 11) * 2 ) ) - 1 ) )')
+
+    # Sample
+    # def test_number(self):
+    #     self.checker(expr)
+
+if __name__ == '__main__':
+    main()
