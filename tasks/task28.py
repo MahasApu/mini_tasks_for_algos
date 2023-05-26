da5327ce0f0d050d301375ff9f412da522473b3e
MahasApu <maryvask0@yandex.ru>
2023-05-26 20:47:08 +0700

    add solution (check for bits per item_

--- tasks/task28.py
+++ tasks/task28.py
@@ -1,71 +1,59 @@
-from math import log, ceil, e
-from bitarray import bitarray
+import math
 from random import randint
-from typing import Callable, List
+from bitset import BitSet
 
 
-class BloomFilter(object):
-    '''
-    Class for Bloom filter, using randomly generated hash function
-    '''
+class Bloom_filter:
+    def __init__(self, n):
+        self.array = BitSet(n)
+        self.universal_hash_funcs = {}
 
-    def __init__(self, items_amount: int, prospect: float):
-        self.capacity = ceil(items_amount * log(e, 0.5) * log(prospect, 2))
-        self.k = ceil(log(2) * self.capacity / items_amount)
-        self.universal_hash = self.get_hash_funcs(self.capacity, self.k)
-        self.bit_array = bitarray(self.capacity)
-        self.bit_array.setall(0)
-
-    def get_hash_funcs(self, capacity: int, hash_amount: int) -> List[Callable]:
-        #
-        def hash_func(rand_num: int, capacity):
-            def get_hash(num):
-                return (num * rand_num) % capacity
-
-            return get_hash
-
-        def hash_generator(capacity, hash_amount):
-            funcs = []
-            for _ in range(hash_amount):
-                funcs.append(hash_func(randint(1, 10000), capacity))
-            return funcs
-
-        return hash_generator(capacity, hash_amount)
-
-    def get_ip_hash(self, ip_address):
-
-        ip_addr = [int(_) for _ in ip_address.split('.')]
-        for i in range(len(ip_addr)):
-            if ip_addr[i] == 0:
-                ip_addr[i] = 1
-
-        return ip_addr[0] * ip_addr[1] * ip_addr[2] * ip_addr[3]
-
-    def insert(self, item) -> None:
-        ip_adr = self.get_ip_hash(item)
-        for func in self.universal_hash:
-            index = func(ip_adr)
-            self.bit_array[index] = 1
-
-    def look_up(self, item) -> bool:
-        ip_adr = self.get_ip_hash(item)
-        for func in self.universal_hash:
-            index = func(ip_adr)
-            if not self.bit_array[index]:
-                return False
-        return True
-
-    @classmethod
-    def next_prime_num(self, num: int) -> int:
-        i = num + 1
-        while True:
-            isPrime = True
-            d = 2
-            while d * d <= i:
-                if i % d == 0:
-                    isPrime = False
-                    break
-                d += 1
-            if isPrime:
-                return i
+    def hash_f(self, k, n):
+        i = 1
+        while k > 0:
+            c0, c1, c2, c3 = randint(1, n - 1), randint(1, n - 1), randint(1, n - 1), randint(1, n - 1)
+            self.universal_hash_funcs[f"h{i}"] = [c0, c1, c2, c3]
             i += 1
+            k -= 1
+
+    def insert(self, id: str, n):
+        parts_id = [int(x) for x in id.split('.')]
+        for j in self.universal_hash_funcs.values():
+            h = (j[0] * parts_id[0] + j[1] * parts_id[1] + j[2] * parts_id[2] + j[3] * parts_id[3]) % n
+            self.array[h] = 1
+
+    def lookup(self, id: str, n):
+        parts_id = [int(x) for x in id.split('.')]
+        flag = True
+        for j in self.universal_hash_funcs.values():
+            h = (j[0] * parts_id[0] + j[1] * parts_id[1] + j[2] * parts_id[2] + j[3] * parts_id[3]) % n
+            if self.array[h] == 0:
+                flag = False
+        if flag:
+            return "true"
+        else:
+            return "false"
+
+
+if __name__ == "__main__":
+    amount = 5
+    probability = 0.99
+    k = math.log(probability, 1 / 2)  # hash func amount
+    b = k / math.log1p(2)  # bit per item
+    print(k, b)
+    n = int(amount * b)  # len of array in  Bloom_filter
+    if n == 0:
+        n = amount
+    print(n)
+    bloom_filter = Bloom_filter(n)
+    bloom_filter.hash_f(k, n)
+    bloom_filter.insert("193.24.134.56", n)
+    bloom_filter.insert("194.54.74.96", n)
+    bloom_filter.insert("23.25.134.86", n)
+    bloom_filter.insert("52.26.124.56", n)
+    bloom_filter.insert("193.27.134.56", n)
+    print(bloom_filter.lookup("194.54.74.96", n))
+    print(bloom_filter.lookup("193.27.134.56", n))
+    print(bloom_filter.lookup("197.54.74.96", n))
+    print(bloom_filter.lookup("177.54.74.96", n))
+    print(bloom_filter.lookup("177.253.74.96", n))
